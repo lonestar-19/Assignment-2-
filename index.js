7354ea4a0b56f6663dc6f94df293017ad25ba36e
Samkibe <kibesam433@gmail.com>
2024-04-03 09:00:52 +0300

    Assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+document.getElementById('scoreForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    let score = e.target.score.value;
+    let grade = '';
+    if (score >= 90 && score <= 100) {
+    grade = 'A';
+    } else if (score >= 80 && score < 90) {
+    grade = 'B';
+    } else if (score >= 70 && score < 80) {
+    grade = 'C';
+    } else if (score >= 60 && score < 70) {
+    grade = 'D';
+    } else if (score >= 0 && score < 60) {
+    grade = 'E';
+    } else {
+    grade = 'Invalid score';
+    }
+    document.getElementById('gradeOutput').textContent = grade;
+   });
